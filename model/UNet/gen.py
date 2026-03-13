70696ffb3713bb9e07bc0065006553d1a3f73f86
x1a0qiya <mn124356@163.com>
2026-03-13 11:13:13 +0800

    Update generater

---
+++ model/UNet/gen.py
@@ -0,0 +1,44 @@
+import torch
+import matplotlib.pyplot as plt
+from model import UNet
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+
+image_size = 128
+loaded_model = UNet(in_channels=3, out_channels=3, base_channels=64).to(device)
+
+save_path = "checkpoints/unet_latest.pth" 
+weights = torch.load(save_path, map_location=device, weights_only=True)
+
+loaded_model.load_state_dict(weights)
+loaded_model.eval()
+
+print("Successfully load the U-Net model, start drawing")
+
+batch_size = 1
+num_steps = 50  
+dt = 1.0 / num_steps
+
+with torch.no_grad():
+    x_gen = torch.randn(batch_size, 3, image_size, image_size).to(device)
+    
+    for i in range(num_steps):
+        t = i * dt
+        
+        t_tensor = torch.full((batch_size,), t, device=device)
+
+        v_pred = loaded_model(x_gen, t_tensor)
+        
+        x_gen = x_gen + v_pred * dt
+
+img_tensor = x_gen.cpu()
+img_display = (img_tensor + 1.0) / 2.0
+img_display = torch.clamp(img_display, 0.0, 1.0)
+img_np = img_display[0].permute(1, 2, 0).numpy()
+
+plt.figure(figsize=(6, 6))
+plt.imshow(img_np)
+plt.axis('off')
+plt.title(f"Generated Cat via Flow Matching ({num_steps} steps)")
+plt.show()
